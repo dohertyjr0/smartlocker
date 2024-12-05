87f8c86aa26d5df9a5ebfd56c741d7a1a9002c6e
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 20:28:56 +0000

    Update script.js

--- webserver/webcode/script.js
+++ webserver/webcode/script.js
@@ -1,20 +1,4 @@
-document.querySelectorAll('.locker').forEach(locker => {
-    locker.addEventListener('click', function() {
-      if (this.classList.contains('available')) {
-        this.classList.remove('available');
-        this.classList.add('reserved');
-        this.textContent = 'Reserved';
-        document.getElementById('status-msg').textContent = 'Locker Reserved!';
-      } else {
-        this.classList.remove('reserved');
-        this.classList.add('available');
-        this.textContent = 'Locker';
-        document.getElementById('status-msg').textContent = 'Locker Available!';
-      }
-    });
-  });
-
-  document.getElementById('start-btn').addEventListener('click', function() {
+    document.getElementById('start-btn').addEventListener('click', function() {
     document.getElementById('landing').classList.remove('active');
     document.getElementById('dashboard').classList.add('active');
   });
