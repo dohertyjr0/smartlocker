bf2b92164a3157f02d81b27de0f706cbc4eef640
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 22:05:18 +0000

    Added comments to handleKeyPadInput function

--- arduino/smartLock/handleKeyPadInput.ino
+++ arduino/smartLock/handleKeyPadInput.ino
@@ -2,10 +2,10 @@ void handleKeypadInput(char key) {
   Serial.print("Key Pressed: ");
   Serial.println(key);
 
-  if (key == 'A' && codeIndex > 0) { 
-    enteredCode[codeIndex] = '\0'; 
+  if (key == 'A' && codeIndex > 0) { //if 'A' key is pressed AND the current key position is greater than 0 ie. 1 key
+    enteredCode[codeIndex] = '\0'; // adds terminate string
     Serial.print("Entered PIN: ");
-    Serial.println(enteredCode);
+    Serial.println(enteredCode); //prints the code entered to serial monitor(was placed for debugging purposes)
 
     if (String(enteredCode) == currentPassword) {
       Serial.println("Admin Mode Access Granted");
@@ -17,14 +17,14 @@ void handleKeypadInput(char key) {
       delay(2000);
       displayLCD("Welcome Back", "Door Locked");
     }
-
-    memset(enteredCode, 0, sizeof(enteredCode));
-    codeIndex = 0;
+    
+    memset(enteredCode, 0, sizeof(enteredCode)); //memset is a built in c function which is primarily used for modifying arrays, here i used it to reset the array 
+    codeIndex = 0; //seperately reset my way of tracking key positions
   } else if (key == 'C' && codeIndex > 0) { 
-    codeIndex--;
-    lcd.setCursor(codeIndex, 1);
-    lcd.print(' ');
-    Serial.println("Last digit cleared.");
+    codeIndex--; //the char 'C' is intended as a backspace, hence --
+    lcd.setCursor(codeIndex, 1); //this techincally prints to the lcd the current key position {3, 2, 1, 0}
+    lcd.print(' '); //provides empty whitespace as placeholder for previous characters entered
+    Serial.println("Last digit cleared."); //Was used as debugging purpose, due to issues with whitespace
   } else if (key != 'A' && key != 'C' && codeIndex < 4) { 
     enteredCode[codeIndex] = key;
     lcd.setCursor(codeIndex, 1);
