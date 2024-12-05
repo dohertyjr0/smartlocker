b06593af82a0c5b42aba7c3689c82a9327e63b58
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 22:24:14 +0000

    comments added

--- arduino/smartLock/checkRFID.ino
+++ arduino/smartLock/checkRFID.ino
@@ -1,18 +1,18 @@
 void checkRFID() {
   uint8_t uid[7];
   uint8_t uidLength;
-
-  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 50)) {
+// used readMifareClassic example from RFID library
+  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 50)) { //50 in this case indicates how long (in ms) how long function waits for tag to be detected
     String scannedUID = "";
     for (uint8_t i = 0; i < uidLength; i++) {
-      scannedUID += String(uid[i], HEX);
+      scannedUID += String(uid[i], HEX); //appending string to hex
     }
     Serial.print("Scanned RFID UID: ");
     Serial.println(scannedUID);
     if (scannedUID == allowedUID) {
       unlockDoor();
       displayLCD("Access Granted", "Locker Unlocked");
-      lastUnlockTime = millis();
+      lastUnlockTime = millis(); //closes lock after 100000 milliseconds
     } else if(scannedUID != allowedUID){
       displayLCD("Card unauthorised", "Incorrect Lock");
       delay(2000);
