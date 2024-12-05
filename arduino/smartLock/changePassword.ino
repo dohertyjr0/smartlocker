12a21455f318c1ff9891152e44e9741ff57dac49
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 22:29:44 +0000

    added final comments

--- arduino/smartLock/changePassword.ino
+++ arduino/smartLock/changePassword.ino
@@ -1,17 +1,17 @@
 void changePassword() {
   displayLCD("Enter New PIN", "");
-  String newPassword = "";
+  String newPassword = ""; //since "String" is techincally an object, was able to use .length() method 
 
   while (newPassword.length() < 4) {
     char key = keypad.getKey();
     if (key) {
       newPassword += key;
-      lcd.setCursor(newPassword.length() - 1, 1);
+      lcd.setCursor(newPassword.length() - 1, 1); //added -1 as a simple way to make sure its being type in the correct key position *on lcd*
       lcd.print('*');
     }
   }
 
-  currentPassword = newPassword;
+  currentPassword = newPassword; //stores our new password in a new string
   displayLCD("PIN Changed", ""); 
   digitalWrite(YELLOW_LED_PIN, HIGH); 
   delay(1000);
