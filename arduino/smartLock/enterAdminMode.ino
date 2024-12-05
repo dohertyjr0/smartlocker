a87edb2dc02f5ce62fc14341c4c8c1cee982f884
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 22:15:36 +0000

    comments

--- arduino/smartLock/enterAdminMode.ino
+++ arduino/smartLock/enterAdminMode.ino
@@ -1,16 +1,16 @@
 void enterAdminMode() {
-  char adminKey = '\0'; 
+  char adminKey = '\0'; // initially i had set this to 0 not realising since this is a char ' ' not realising whitespace has an ASCII value, ensures adminKey is set to null
 
   while (true) {
     displayLCD("Admin Mode", "1:Change PIN");
     digitalWrite(RED_LED_PIN, LOW);   
     digitalWrite(WHITE_LED_PIN, LOW);  
     digitalWrite(YELLOW_LED_PIN, HIGH); 
-    adminKey = keypad.getKey();
+    adminKey = keypad.getKey(); // stores keypresses in adminKey
 
     if (adminKey) {
       Serial.print("Admin Key Pressed: ");
-      Serial.println(adminKey);
+      Serial.println(adminKey); 
 
       if (adminKey == '1') {
         Serial.println("Changing PIN...");
