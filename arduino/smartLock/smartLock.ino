c251e0e81119ac817700e9a45dd5bcd0263a3503
dohertyjr0 <dohertyjr0@icloud.com>
2024-12-05 20:51:20 +0000

    Update smartLock.ino

--- arduino/smartLock/smartLock.ino
+++ arduino/smartLock/smartLock.ino
@@ -6,12 +6,8 @@
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 
-const char* ssid = "iPhone 12 Pro";
-const char* password = "01234567";
-#define FIREBASE_HOST "https://esp32-database-5fc3b-default-rtdb.europe-west1.firebasedatabase.app/"
-#define FIREBASE_AUTH ""
-
-FirebaseData firebaseData;
+const char* SSID = "iPhone 12 Pro";
+const char* PASSWORD= "01234567";
 
 #define PN532_SDA 21
 #define PN532_SCL 22
@@ -33,11 +29,12 @@ byte colPins[COLS] = { 17, 25, 26, 33 };
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 Adafruit_PN532 nfc(PN532_SDA, PN532_SCL);
 Servo myServo;
+
 rgb_lcd lcd;
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
 
-bool locked = true;
+bool lockedServo = true;
 char enteredCode[5];
 int codeIndex = 0;
 String currentPassword = "1234";
@@ -50,7 +47,7 @@ const unsigned long rfidCheckInterval = 2000;
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to Wi-Fi...");
@@ -123,7 +120,7 @@ void displayMessage(const char* line1, const char* line2) {
 }
 
 void unlockDoor() {
-  locked = false;
+  lockedServo = false;
   myServo.write(270);
   digitalWrite(RED_LED_PIN, LOW);
   digitalWrite(WHITE_LED_PIN, HIGH);
@@ -131,7 +128,7 @@ void unlockDoor() {
 }
 
 void lockDoor() {
-  locked = true;
+  lockedServo = true;
   myServo.write(90);
   digitalWrite(RED_LED_PIN, HIGH);
   digitalWrite(WHITE_LED_PIN, LOW);
