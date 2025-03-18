c614d7f3c1e955679b7edf0bee0c453ca95eee10
doheryjry0 <dohertyjr0@icloud.com>
2025-03-18 13:58:02 +0000

    Fixed spelling error

--- arduino/smartLock/webapp.ino
+++ arduino/smartLock/webapp.ino
@@ -74,7 +74,7 @@ String webApp(){
         function fetchRFID(){
           fetch('/scannedUID')
           .then(response => response.text())
-          .then(data => document.getElementbyID("rfidUID")).innerText = data)
+          .then(data => document.getElementbyId("rfidUID")).innerText = data)
           .catch(error => console.error('Error:', error));
         }
 
