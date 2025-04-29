45f13ca0fa65688dd640e0be9a43623a630051e2
JohnRoss Doherty <dohertyjr0@icloud.com>
2025-04-29 20:41:09 +0100

    Replacement elif with switch

--- arduino/smartLock/handleKeyPadInput.ino
+++ arduino/smartLock/handleKeyPadInput.ino
@@ -2,44 +2,48 @@ void handleKeypadInput(char key) {
   Serial.print("Key Pressed: ");
   Serial.println(key);
 
-  if(key == 'A'){
-    lockDoor();
-    displayLCD("Locked", "Manual Override");
-    memset(enteredCode, 0, sizeof(enteredCode));
-    codeIndex = 0;
-    return;
-  }
-  
-  if (key == 'A' && codeIndex > 0) { //if 'A' key is pressed AND the current key position is greater than 0 ie. 1 key
-    enteredCode[codeIndex] = '\0'; // adds terminate string
-    Serial.print("Entered PIN: ");
-    Serial.println(enteredCode); //prints the code entered to serial monitor(was placed for debugging purposes)
+  switch (key) {
 
-    if (String(enteredCode) == currentPassword) {
-      Serial.println("Admin Mode Access Granted");
-      enterAdminMode();
-      unlockDoor();
-    } else {
-      Serial.println("Incorrect PIN!");
-      displayLCD("Incorrect Pin!", "Try Again");
-      delay(2000);
-      displayLCD("Welcome Back", "Door Locked");
-    }
-    
-    memset(enteredCode, 0, sizeof(enteredCode)); //memset is a built in c function which is primarily used for modifying arrays, here i used it to reset the array 
-    codeIndex = 0; //seperately reset my way of tracking key positions
-  } else if (key == 'C' && codeIndex > 0) { 
-    codeIndex--; //the char 'C' is intended as a backspace, hence --
-    lcd.setCursor(codeIndex, 1); //this techincally prints to the lcd the current key position {3, 2, 1, 0}
-    lcd.print(' '); //provides empty whitespace as placeholder for previous characters entered
-    Serial.println("Last digit cleared."); //Was used as debugging purpose, due to issues with whitespace
-  } else if (key != 'A' && key != 'C' && codeIndex < 4) { 
-    enteredCode[codeIndex] = key;
-    lcd.setCursor(codeIndex, 1);
-    lcd.print('*'); 
-    codeIndex++;
-    
-    Serial.print("Current PIN Entry: ");
-    Serial.println(enteredCode);
+    case 'A': 
+      lockDoor();
+      displayLCD("Locked", "Manual Override");
+      memset(enteredCode, 0, sizeof(enteredCode));
+      codeIndex = 0;
+      return;
+
+    case 'C': 
+      if (codeIndex > 0) {
+        codeIndex--;
+        lcd.setCursor(codeIndex, 1);
+        lcd.print(' ');
+        lcd.setCursor(codeIndex, 1);
+      }
+      return;
+
+    default: 
+      if (codeIndex < 4 && isDigit(key)) {
+        enteredCode[codeIndex++] = key;
+        lcd.setCursor(codeIndex - 1, 1);
+        lcd.print('*');
+      }
+
+      if (codeIndex == 4) {
+        enteredCode[4] = '\0'; 
+        String entered = String(enteredCode);
+
+        if (entered == currentPassword) {
+          unlockDoor();
+          displayLCD("Unlocked", "Welcome");
+          delay(3000);
+        } else {
+          displayLCD("Wrong PIN", "Try Again");
+          delay(2000);
+        }
+
+        memset(enteredCode, 0, sizeof(enteredCode));
+        codeIndex = 0;
+        displayLCD("Enter PIN", "");
+      }
   }
 }
+
